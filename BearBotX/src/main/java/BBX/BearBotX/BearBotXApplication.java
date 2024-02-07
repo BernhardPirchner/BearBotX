c6051bd4905e85601227744edabf63d8b70ca400
BernhardPirchner <bernhard.pirchner@htl-saalfelden.at>
2024-02-07 09:59:18 +0100

    Backend Start
    Spring Boot Initialisation

---
+++ BearBotX/src/main/java/BBX/BearBotX/BearBotXApplication.java
@@ -0,0 +1,13 @@
+package BBX.BearBotX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearBotXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BearBotXApplication.class, args);
+	}
+
+}
---
+++ BearBotX/src/test/java/BBX/BearBotX/BearBotXApplicationTests.java
@@ -0,0 +1,13 @@
+package BBX.BearBotX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BearBotXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
