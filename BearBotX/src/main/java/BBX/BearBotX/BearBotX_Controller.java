560ab192dbbbce86d0ec062f33a1c88493f26c0f
BernhardPirchner <bernhard.pirchner@htl-saalfelden.at>
2024-02-21 08:22:38 +0100

    Backend 1
    -Initialized Spring-Boot REST Project
    -added several Endpoints which will be needed
    -added one Endpoint for Testing the Connection to the Backend

--- BearBotX/src/main/java/BBX/BearBotX/BearBotX_Controller.java
+++ BearBotX/src/main/java/BBX/BearBotX/BearBotX_Controller.java
@@ -1,18 +1,42 @@
 package BBX.BearBotX;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 class BearBotX_Controller {
 
+    Client client=null;
     private int i=8;
 
     @GetMapping("/test")
-    int GetInt(){
+    int getInt(){
         return i;
     }
 
 
+    @PostMapping("/connect")
+    public String connectToServer(@RequestBody String ip){
+        try{
+            client=new Client(ip);
+        }catch(Exception ex){
+            return ex.getMessage();
+        }
+        return "Connection Successful";
+    }
 
+    @PostMapping("/send")
+    public void send(@RequestBody String command){
+        client.send(command);
+    }
+
+    @GetMapping("/data")
+    public String getData(){
+        return client.getData();
+    }
+
+    @PostMapping("/velocity")
+    public void setVelocity(@RequestBody int speed){
+        client.setSpeed(speed);
+    }
 }
---
+++ BearBotX/src/main/java/BBX/BearBotX/Client.java
@@ -0,0 +1,61 @@
+package BBX.BearBotX;
+
+import java.io.PrintWriter;
+import java.net.*;
+import java.util.Scanner;
+
+public class Client {
+    Socket server=null;
+    Scanner in=null;
+    PrintWriter out=null;
+
+    public Client(String ip) {
+        try{
+            server=new Socket(ip, 6969);
+            in=new Scanner(server.getInputStream());
+            out=new PrintWriter(server.getOutputStream());
+        }catch(Exception ex){
+            System.out.println("Hallo"+ ex.getMessage());
+        }
+    }
+
+    /*
+    public void Listen(){
+        System.out.println(in.nextLine());
+    }
+    */
+
+    public String send(String s){
+        String answer="No Answer";
+        if(server != null){
+            try{
+                out.println(s);
+                answer= in.nextLine();
+                server.close();
+            }catch(Exception ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+        return answer;
+    }
+
+    public String getData(){
+        out.println("hole Daten");
+        return in.nextLine();
+    }
+
+    public void setSpeed(int velocity){
+        out.println("ändere Geschwindigkeit zu: "+velocity);
+    }
+
+    private void closeConnection(){
+        if(server!=null){
+            try{
+                server.close();
+            }catch(Exception ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+}
