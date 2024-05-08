6a6c2f6dad3da309a9ca06dd2f9574f712942800
HirschLPYT <duernbergermanuel2@gmail.com>
2024-05-08 08:27:37 +0200

    Code Cleanup and Commenting

--- MBot_Code/BotScript.py
+++ MBot_Code/BotScript.py
@@ -12,7 +12,8 @@ acceptCommands = True
 autoPilot = False
 
 
-def safeFunc(active):  # Function for Safe Mode Thread
+## Function for Safe Mode Thread
+def safeFunc(active):
     print("Safety Thread Started")
     global acceptCommands
     global autoPilot
@@ -27,7 +28,7 @@ def safeFunc(active):  # Function for Safe Mode Thread
                 cyberpi.mbot2.straight(-10, speed=100)  # Drive Back 10 cm
                 cyberpi.mbot2.turn(180, speed=100)  # Turn around
                 acceptCommands = True  # Continue Main thread receiving Commands
-        if autoPilot:  # ----------DEPRECATED NEEDS TO BE REWRITTEN DUE TO SPECIFICATIONS
+        if autoPilot:  # ----------DEPRECATED >>> NEEDS TO BE REWRITTEN DUE TO SPECIFICATIONS
             # L2 = cyberpi.quad_rgb_sensor.get_gray('l2', index = 1)
             L1 = cyberpi.quad_rgb_sensor.get_gray('l1', index=1)
             R1 = cyberpi.quad_rgb_sensor.get_gray('r1', index=1)
@@ -45,7 +46,8 @@ def safeFunc(active):  # Function for Safe Mode Thread
                 # callable.led.on(255,0,0,id=4)
 
 
-def main():  # Main Function
+## Main Function
+def main():
     cyberpi.audio.set_vol(1)
     cyberpi.led.on(0, 0, 0xFF)
 
@@ -65,8 +67,8 @@ def main():  # Main Function
             cyberpi.audio.play("ring")
             break
 
-    ip = cyberpi.network.get_ip()
-    port = 6969
+    ip = cyberpi.network.get_ip()  # assigned IP-Adress
+    port = 6969  # Communication Port
 
     cyberpi.console.println(ip)
     time.sleep(5)
@@ -74,26 +76,26 @@ def main():  # Main Function
     cyberpi.console.clear()
     cyberpi.led.off()
 
+    # Socket for Broadcast message using UDP
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     ai = socket.getaddrinfo(ip, port)
-
-    addr = ai[0][-1]
-
+    addr = ai[0][-1]  # formatted IP-Adress
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
     s.bind(addr)
-    s.sendto(ip, ("10.10.3.255", 6970))
+    s.sendto(ip, ("10.10.3.255", 6970))  # Broadcast
 
-    cyberpi.console.println("Waiting for Connections")
-    cyberpi.led.on(255, 31, 0)
+    cyberpi.console.println("Waiting for Connection")
+    cyberpi.led.on(255, 64, 0)
 
+    # Socket for Communication Server using TCP
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
     server_socket.bind(addr)
 
+    # Start Thread for Safety Mode and Autopilot
     _thread.start_new_thread(safeFunc, [safetyMode])
 
+    # listen for Connections
     server_socket.listen(5)
     while True:
         # Client Connection
@@ -101,16 +103,14 @@ def main():  # Main Function
         print("c_socket:", client_sock, "\nc_raddr:", client_raddr)
         print("Client Address:", client_raddr)
         print("Client Socket:", client_sock)
-        cyberpi.console.println("Connected to:", client_raddr)
+        cyberpi.console.println("Connected to:" + client_raddr)
 
+        # Continuously get Commands from Client
         while True:
-            # Continuously get Commands from Client
-            data = client_sock.recv(1024)
-            dataframe = data.decode('utf-8').split(';')[1]
-            command, extra, data = dataframe.split(':')
-            sepData = data.split(',')
-            print("###INCOMING COMMAND###")
-            print("Dataframe", dataframe)
+            data = client_sock.recv(1024)  # Data from Client
+            dataframe = data.decode('utf-8').split(';')[1]  # Stacked Command Protection
+            command, extra, data = dataframe.split(':')  # Extracted Command Data
+            sepData = data.split(',')  # Seperated Data Values
             print(command, ":", extra, ":", data)
 
             if command == "MOVE" and acceptCommands == True:
@@ -119,36 +119,30 @@ def main():  # Main Function
                 angle = int(sepData[1])
                 style = sepData[2]
 
-                # print("Recieved Movement Command")
                 if extra == "STOP":  # Stop the Robot
                     print("STOP Commanded")
                     cyberpi.mbot2.EM_stop(port="all")
                     cyberpi.led.on(0xFF, 0x00, 0x00, id=1)
                     cyberpi.led.on(0xFF, 0x00, 0x00, id=5)
                 elif extra == "FWST":  # Move Forward in a straight line
-                    print("Forward Commanded")
                     print(sepData[0])
                     cyberpi.led.off()
                     cyberpi.mbot2.forward(speed)
                 elif extra == "BWST":  # Move Backward in a straight line
-                    print("Backward Commanded")
                     print(sepData[0])
                     cyberpi.led.off()
                     cyberpi.led.on(0xFF, 0xFF, 0xFF, id=2)
                     cyberpi.led.on(0xFF, 0xFF, 0xFF, id=4)
                     cyberpi.mbot2.backward(speed)
                 elif extra == "TRLT":  # Turn left on the spot
-                    print("Turn Left Commanded")
                     cyberpi.led.off()
                     cyberpi.mbot2.drive_power(-speed, -speed)
                 elif extra == "TRRT":  # Turn right on the spot
-                    print("Turn Right Commanded")
                     cyberpi.led.off()
                     cyberpi.mbot2.drive_power(speed, speed)
                 elif extra == "FWLT":  # Move Forward and turn left,
                     # ratio determined by angle imput [0-90]->[0.0-1.0]
                     # RPM Ratio = factor/1
-                    print("Turn Forward Left Commanded")
                     cyberpi.led.off()
                     factor = angle / 90
                     ltdrv = speed * factor
@@ -156,7 +150,6 @@ def main():  # Main Function
                 elif extra == "FWRT":  # Move Forward and turn right,
                     # ratio determined by angle imput [0-90]->[0.0-1.0]
                     # RPM Ratio = factor/1
-                    print("Turn Forward Right Commanded")
                     cyberpi.led.off()
                     factor = angle / 90
                     rtdrv = speed * factor
@@ -164,7 +157,6 @@ def main():  # Main Function
                 elif extra == "BWLT":  # Move Backward and turn left,
                     # ratio determined by angle imput [0-90]->[0.0-1.0]
                     # RPM Ratio = factor/1
-                    print("Turn Backward Left Commanded")
                     cyberpi.led.off()
                     cyberpi.led.on(0xFF, 0xFF, 0xFF, id=2)
                     factor = angle / 90
@@ -173,17 +165,14 @@ def main():  # Main Function
                 elif extra == "BWRT":  # Move Backward and turn right,
                     # ratio determined by angle imput [0-90]->[0.0-1.0]
                     # RPM Ratio = factor/1
-                    print("Turn Backward Right Commanded")
                     cyberpi.led.off()
                     cyberpi.led.on(0xFF, 0xFF, 0xFF, id=4)
                     factor = angle / 90
                     rtdrv = speed * factor
                     cyberpi.mbot2.drive_power(-speed, rtdrv)
-
             elif command == "MISC":
                 # Code for miscalanious Commands
-                if extra == "LEDS":
-                    # Changing LEDS
+                if extra == "LEDS":  # Changing LEDS
                     r = int('0x' + sepData[0], 16)
                     g = int('0x' + sepData[1], 16)
                     b = int('0x' + sepData[2], 16)
@@ -192,36 +181,22 @@ def main():  # Main Function
                     else:
                         ID = int(sepData[3])
                         cyberpi.led.on(r, g, b, id=ID)
-
                 elif extra == "SAFE":
                     # Toggle Safety Mode
-                    # cyberpi.console.print("Toggled Safetymode")
-                    print("Safety toggle")
                     if safetyMode == True:
                         safetyMode = False
                     else:
                         safetyMode = True
-
                     autoPilot = False
-                    print(safetyMode)
-                    # cyberpi.console.clear()
-                    # cyberpi.console.print("Safe:"+str(safetyMode)+",Pilot:"+str(autoPilot))
 
                 elif extra == "AUTO":
                     # Toggle Autopilot
-                    cyberpi.console.print("Toggled Autopilot")
-                    print("Autopilot toggle")
                     autoPilot = True
-                    print(autoPilot)
-                    # cyberpi.console.clear()
-                    # cyberpi.console.print("Safe:"+str(safetyMode)+",Pilot:"+str(autoPilot))
 
             elif command == "DATA":
                 # Code for sending Data back to the client
                 dataJson = getSensorData()
-                # print(dataJson)
                 client_sock.send(dataJson)
-                print("Data sent to client")
 
             elif command == "DISC":
                 cyberpi.mbot2.EM_stop(port="all")
@@ -229,29 +204,31 @@ def main():  # Main Function
 
         cyberpi.led.off()
         cyberpi.mbot2.EM_stop(port="all")
-        cyberpi.console.clear()
+        cyberpi.console.print("Waiting for Connection")
+        cyberpi.led.on(0xFF, 0x40, 0x00)
         client_sock.close()
 
 
+## Get Data from Sensors and return formatted JSON
 def getSensorData():
-    light = cyberpi.get_brightness()
-    volume = cyberpi.get_loudness()
-    pitch = cyberpi.get_pitch()
-    roll = cyberpi.get_roll()
-    yaw = cyberpi.get_yaw()
-    distance = cyberpi.ultrasonic2.get(index=1)
-    battery = cyberpi.get_battery
-    gyrox = str(cyberpi.get_gyro("x"))
-    gyroy = str(cyberpi.get_gyro("y"))
-    gyroz = str(cyberpi.get_gyro("z"))
+    light = cyberpi.get_brightness()  # brightness
+    volume = cyberpi.get_loudness()  # audio volume
+    pitch = cyberpi.get_pitch()  # pitch ofset from initial position
+    roll = cyberpi.get_roll()  # roll ofset from initial position
+    yaw = cyberpi.get_yaw()  # yaw ofset from initial position
+    distance = cyberpi.ultrasonic2.get(index=1)  # distance from ultrasonic
+    battery = cyberpi.get_battery  # battery percentage
+    gyrox = str(cyberpi.get_gyro("x"))  # pitch acceleration
+    gyroy = str(cyberpi.get_gyro("y"))  # roll acceleration
+    gyroz = str(cyberpi.get_gyro("z"))  # yaw acceleration
     RGBSensorData = cyberpi.quad_rgb_sensor.get_line_sta(index=1)
     LineS = '{0:04b}'.format(RGBSensorData)
-    # print(LineS)
-    L2 = LineS[0]
-    L1 = LineS[1]
-    R1 = LineS[2]
-    R2 = LineS[3]
+    L2 = LineS[0]  # outer left Sensor
+    L1 = LineS[1]  # inner left Sensor
+    R1 = LineS[2]  # inner right Sensor
+    R2 = LineS[3]  # outer right Sensor
 
+    # JSON string
     json_string = jsonlib.dumps(
         {'light': light, 'battery': battery, 'volume': volume, 'gyrox': gyrox, 'gyroy': gyroy, 'gyroz': gyroz,
          'pitch': pitch, 'roll': roll, 'yaw': yaw, 'distance': distance, 'L1': L1, 'L2': L2, 'R1': R1, 'R2': R2})
@@ -259,6 +236,7 @@ def getSensorData():
     return json_string
 
 
+## Smooth acceleration
 def moveFW(time):
     for i in range(20):
         t = i / 10.0
@@ -273,12 +251,13 @@ def moveFW(time):
     cyberpi.console.clear()
 
 
+### Main
 try:
     main()
-except BaseException as ex:
+except BaseException as ex:  # Catch Error
     cyberpi.led.off()
     print("ERROR:", ex)
-    cyberpi.console.print("Error: ", ex)
+    cyberpi.console.println("Error")
+    cyberpi.console.print(ex)
     cyberpi.mbot2.EM_stop(port="all")
-    # cyberpi.console.clear()
 
